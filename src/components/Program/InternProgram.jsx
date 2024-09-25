2a1add40a63aa7687ee026ccaf8ff32ce44303d4
SheerGhazi <sher.ghazi2192@gmail.com>
2024-09-25 22:28:45 +0500

    fix: progtam Page

--- src/components/Program/InternProgram.jsx
+++ src/components/Program/InternProgram.jsx
@@ -28,7 +28,7 @@ const InternProgramData = [
 
 const InternProgram = () => {
   return (
-    <div className='w-full h-auto bg-gradient-to-b from-[#c5f5f7] to-[#d6f2f3] flex flex-col text-center gap-10 '>
+    <div className='w-full h-auto bg-gradient-to-b from-[#c5f5f7] to-[#d6f2f3] flex flex-col text-center gap-10 py-10 '>
       <div className="heading text-blue-400 font-semibold text-3xl">INTERNSHIP PROGRAMS</div>
       <div className="cards flex flex-col justify-evenly md:flex-row ">
         {InternProgramData.map((intern, index) => (
