4415ad2683f3b1e4e05c90f1a55a0420ed9b706d
SheerGhazi <sher.ghazi2192@gmail.com>
2024-09-26 12:45:12 +0500

    fix : Traini2

--- src/components/Program/Traine2.jsx
+++ src/components/Program/Traine2.jsx
@@ -10,21 +10,21 @@ const Traine2 = () => {
          Work with experts, grow your skills, and shape the future of digital solutions
 
        </h3>
-     <div className="w-full md:flex  px-12 mt-5  ">
+     <div className="w-full md:flex px-20 mt-5  ">
       
        <div className="right-ctn w-full p-6 md:w-1/2 md:flex justify-center items-center " >
            <img className="cover w-auto h-[70vh] rounded-3xl" src={Right_Image} alt="" />
        </div>
 
        
-       <div className="left-ctn w-full p-6 space-y-4  md:w-1/2   ">
+       <div className="left-ctn w-full p-6  space-y-4 flex flex-col justify-center  md:w-1/2  md:pr-40  ">
          <h1 className="font-bold text-4xl  text-black">TRAINEE PROGRAMS</h1>
-         <p className="font-normal text-lg text-gray-900">
+         <p className="font-normal text-base text-gray-900">
            We specialize in delivering cutting-edge IT services tailored to
            meet the unique needs of businesses across industries. Our expert
            team is dedicated to providing .
          </p>
-         <ul className="text-lg font-bold">
+         <ul className="text-lg font-semibold">
            <li>App Development</li>
            <li>Digital Marketing</li>
            <li>AI Solutions</li>
