3fdfa5f04e70525401e972f277de42a5c4622bde
SheerGhazi <sher.ghazi2192@gmail.com>
2024-09-26 12:49:54 +0500

    fix: taini comp

--- src/components/Program/Traine.jsx
+++ src/components/Program/Traine.jsx
@@ -9,16 +9,16 @@ const Traine = () => {
           Work with experts, grow your skills, and shape the future of digital
           solutions
         </h3>
-      <div className="w-full md:flex px-12 mt-5  ">
+      <div className="w-full md:flex px-20 mt-5  ">
        
-        <div className="left-ctn w-full p-6 space-y-4  md:w-1/2 ">
+        <div className="left-ctn w-full flex flex-col justify-center p-6 space-y-4  md:w-1/2 px-20 ">
           <h1 className="font-bold text-4xl  text-black">TRAINEE PROGRAMS</h1>
-          <p className="font-normal text-lg text-gray-900">
+          <p className="font-normal text-base text-gray-900">
             We specialize in delivering cutting-edge IT services tailored to
             meet the unique needs of businesses across industries. Our expert
             team is dedicated to providing .
           </p>
-          <ul className="text-lg font-bold">
+          <ul className="text-lg font-semibold">
             <li>App Development</li>
             <li>Digital Marketing</li>
             <li>AI Solutions</li>
