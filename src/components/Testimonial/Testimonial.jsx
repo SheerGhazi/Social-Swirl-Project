972d8bc8772bcb8e3c4cceb6d170d438cfb73837
SheerGhazi <sher.ghazi2192@gmail.com>
2024-09-28 17:57:36 +0500

    fix : Testimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import cln_img from "../../assets/Images/Cln-img.png"
 
 const Testimonial = () => {
   return (
@@ -13,30 +14,31 @@ const Testimonial = () => {
               We've helped our clients...
             </h2>
 
-            <div className="flex flex-col md:flex-row items-stretch">
+            <div className="flex flex-col gap-4 md:flex-row items-stretch">
               <div className="md:w-1/3 mb-4 md:mb-0 flex items-stretch">
                 <img
-                  src="src/assets/Images/image4.webp"
+                  src={cln_img}
                   alt="Client with certificate"
                   className="w-full h-full object-cover"
                 />
               </div>
-              <div className="md:w-4/6 md:pl-8 flex flex-col justify-between">
-                <div className="bg-gray-700 text-white p-6 h-full">
+              <div className="bg-black opacity-45 text-white p-2 flex flex-col justify-between  md:w-4/6 md:p-8 ">
+                <div className="">
                   <h3 className="text-2xl font-semibold mb-4">
                     Rescue their projects
                   </h3>
-                  <p className="mb-4">
+                  <p className="mb-4 text-[#EDEDED99] opacity-85">
                     Lorem ipsum dolor sit amet consectetur. Massa eget urna sed
                     id commodo sed consequat rhoncus. Urna quis leo euismod
                     neque egestas pellentesque. Ac nunc mattis cursus gravida a
                     orci enim. Leo augue in duis mollis orci aliquam enim in.
                   </p>
-                  <div>
-                    <p className="font-semibold">Danial khan</p>
-                    <p className="text-sm">CEO of DK Recruitment</p>
-                  </div>
+                 
                 </div>
+                <div>
+                    <p className="font-semibold">Danial khan</p>
+                    <p className="text-sm tracking-wider text-[#EDEDED99] opacity-85">CEO of DK Recruitment</p>
+                  </div>
               </div>
             </div>
           </div>
